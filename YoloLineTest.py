6d39f4e9c9a42b6104edb3132535e25bf8ae1fe1
MostafaElshamy <mostafaelshamy@aucegypt.edu>
2025-07-09 13:56:15 +0300

    average times

--- YoloLineTest.py
+++ YoloLineTest.py
@@ -28,6 +28,11 @@ with open('calibration.json', 'r') as f:
 cross_threshold = calibration_data.get("threshold", 3)
 
 prev_centroids = []
+avg_time = 0.0
+objects_detected = 0
+time_between_crossings = []
+
+
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -35,26 +40,40 @@ while cap.isOpened():
         break
 
     # Run YOLOv8 inference
+    results = model(frame, verbose=False)[0]
 
-    results = model(frame, verbose=False) 
-    detections = results[0].boxes.xyxy.cpu().numpy()
+    # Get detections: boxes, confidences, class indices
+    boxes = results.boxes.xyxy.cpu().numpy()
+    scores = results.boxes.conf.cpu().numpy()
+    classes = results.boxes.cls.cpu().numpy()
 
     curr_centroids = []
     crossed_this_frame = False
 
-    for x1, y1, x2, y2 in detections:
-        cx, cy = int((x1 + x2) / 2), int((y1 + y2) / 2)
-        curr_centroids.append((cx, cy))
+    
 
-        # Draw bounding box and centroid
-        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 255, 0), 2)
-        cv2.circle(frame, (cx, cy), 5, (0, 255, 0), -1)
+    # COCO bottle index
+    TARGET_CLASSES = [39] #only targeting bottles
 
-        # Line crossing check (left to right)
-        for pcx, pcy in prev_centroids:
-            if pcx < line_x and cx >= line_x: #Currently crossing
-                crossed_this_frame = True
-                last_cross_time = time.time()
+    for box, conf, cls in zip(boxes, scores, classes):
+        if int(cls) in TARGET_CLASSES:
+            x1, y1, x2, y2 = box
+            cx, cy = int((x1 + x2) / 2), int((y1 + y2) / 2)
+            curr_centroids.append((cx, cy))
+
+            # Draw bounding box and centroid
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 255, 0), 2)
+            cv2.circle(frame, (cx, cy), 5, (0, 255, 0), -1)
+
+            # Line crossing check (left to right)
+            for pcx, pcy in prev_centroids:
+                if pcx < line_x and cx >= line_x:
+                    crossed_this_frame = True
+    
+                    if (time.time() - last_cross_time > 1):  # Avoid multiple crossings in a short time   
+                        time_between_crossings.append(time.time() - last_cross_time)
+                    objects_detected += 1
+                    last_cross_time = time.time()
 
     # Draw vertical line
     cv2.line(frame, (line_x, 0), (line_x, height), (0, 0, 255), 2)
@@ -64,19 +83,28 @@ while cap.isOpened():
     production_status = "Running" if time_since_last_cross < cross_threshold else "Stopped"
     color = (0, 255, 0) if production_status == "Running" else (0, 0, 255)
 
-    # Display status at the top-left corner
+    # Display status
     cv2.putText(frame, f"Production: {production_status}", (30, 60),
                 cv2.FONT_HERSHEY_SIMPLEX, 2, color, 3)
 
-    cv2.putText(frame, f"Time since last object: {time_since_last_cross:.2f}", (30,120),cv2.FONT_HERSHEY_SIMPLEX, 2, color, 3)
+    cv2.putText(frame, f"Time since last object: {time_since_last_cross:.2f}", (30, 120),
+                cv2.FONT_HERSHEY_SIMPLEX, 2, color, 3)
+    
+    if objects_detected > 0:
+        avg_time = sum(time_between_crossings) / len(time_between_crossings)
 
-    # Update previous centroids
+    cv2.putText(frame, f"Avg Time Between Crossings: {avg_time:.2f}", (30, 180),
+        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 3)
+
     prev_centroids = curr_centroids
     out.write(frame)
     cv2.imshow("YOLOv8 + DeepSORT Tracking", frame)
+
     if cv2.waitKey(21) & 0xFF == 27:  # ESC to quit
         break
-    
+
+print(np.array(time_between_crossings).std())
+
 
 cap.release()
 out.release()
